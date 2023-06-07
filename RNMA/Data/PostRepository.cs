0efa194d4a576750a6d96552d1dc80777da78384
maicol hernandez <maykolandres4@gmail.com>
2023-06-07 03:17:54 -0500

    done with repository operations

---
+++ RNMA/Data/PostRepository.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RNMA.Data
+{
+    internal static class PostRepository
+    {
+        internal async static Task<List<Post>> GetPostsAsync()
+        {
+            using (var db = new AppDbContext())
+            {
+                return await db.Posts.ToListAsync();    
+            }
+        }
+
+        internal async static Task<Post> GetPostByIdAsync(int postId)
+        {
+            using (var db = new AppDbContext())
+            {
+                return await db.Posts.FirstOrDefaultAsync(post => post.PostId == postId);
+            }
+        }
+
+        internal async static Task<bool> CreatePostAsync(Post postToCreate)
+        {
+            using (var db = new AppDbContext())
+            {
+                try
+                {
+                    await db.Posts.AddAsync(postToCreate);
+
+                    return await db.SaveChangesAsync() > 1;
+                }
+                catch(Exception e) 
+                {
+                    return false;
+                }
+            }
+        }
+
+        internal async static Task<bool> UpdatePostAsync(Post postToUpdate)
+        {
+            using (var db = new AppDbContext())
+            {
+                try
+                {
+                    db.Posts.Update(postToUpdate);
+
+                    return await db.SaveChangesAsync() > 1;
+                }
+                catch (Exception e)
+                {
+                    return false;
+                }
+            }
+        }
+
+        internal async static Task<bool> DeletePostAsync(int postId)
+        {
+            using (var db = new AppDbContext())
+            {
+                try
+                {
+                    Post postToDelete = await GetPostByIdAsync(postId);
+
+                    db.Remove(postToDelete);
+
+                    return await db.SaveChangesAsync() > 1;
+                }
+                catch (Exception e)
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
