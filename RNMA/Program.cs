4219823515940203166e0b407a98ab159915eeb5
maicol hernandez <maykolandres4@gmail.com>
2023-06-09 02:56:33 -0500

    created more endpoints

--- RNMA/Program.cs
+++ RNMA/Program.cs
@@ -21,5 +21,70 @@ app.UseHttpsRedirection();
 //endpoints
 app.MapGet("/get-all-posts", async () => await PostRepository.GetPostsAsync());
 
+//GET with ID
+
+app.MapGet("/get-post-by-id/{postId}", async (int postId) =>
+{
+    Post postToReturn = await PostRepository.GetPostByIdAsync(postId);
+
+    if (postToReturn != null)
+    {
+        return Results.Ok(postToReturn);
+    }
+    else
+    {
+        return Results.BadRequest();
+    }
+});
+
+//POST create post
+
+app.MapPost("/create-post", async (Post postToCreate) =>
+{
+    bool createSuccessful = await PostRepository.CreatePostAsync(postToCreate);
+
+    if (createSuccessful)
+    {
+        return Results.Ok("created successfully");
+    }
+    else
+    {
+        return Results.BadRequest();
+    }
+});
+
+//PUT update post
+
+app.MapPut("/update-post", async (Post postToUpdate) =>
+{
+    bool updateSuccessful = await PostRepository.UpdatePostAsync(postToUpdate);
+
+    if (updateSuccessful)
+    {
+        return Results.Ok("updated successfully");
+    }
+    else
+    {
+        return Results.BadRequest();
+    }
+});
+
+//DELETE delete post
+
+app.MapDelete("/delete-post-by-id/{postId}", async (int postId) =>
+{
+    bool deleteSuccessful = await PostRepository.DeletePostAsync(postId);
+
+    if (deleteSuccessful)
+    {
+        return Results.Ok("deleted successfully");
+    }
+    else
+    {
+        return Results.BadRequest();
+    }
+});
+
+
 app.Run();
 
